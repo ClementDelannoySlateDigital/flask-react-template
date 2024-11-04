c023ecfac371a5a14fa093c6d34769a9618953db
Clément Delannoy <clement.delannoy@slatedigital.dev>
2024-11-04 14:41:01 +0100

    Add API page

--- apps/views.py
+++ apps/views.py
@@ -5,3 +5,7 @@ app_blueprint = Blueprint('app',__name__)
 @app_blueprint.route('/app')
 def index():
 	return render_template("index.html")
+
+@app_blueprint.route('/api')
+def api():
+	return "This is API homepage"
